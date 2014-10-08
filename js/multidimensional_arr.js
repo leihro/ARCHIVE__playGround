bb66da1ffcb7adc43c5628f234343c2a32d9ce06
leiw <lei036072728@gmail.com>
2014-10-08 18:11:37 +0200

    Update and rename test.js to multidimensional_arr.js

---
+++ js/multidimensional_arr.js
@@ -0,0 +1,13 @@
+/**
+ * multidimentional arr in js
+ * 
+ */
+ 
+var arr_level1 = [],
+ 	arr_level2 = arr_level1[1] = [],
+ 	arr_level3 = arr_level2[1] = [],
+ 	arr_level4 = arr_level3[1] = [1,2,3,4,5];
+
+console.log(arr_level1[1][1][1][3]);
+
+ 	
